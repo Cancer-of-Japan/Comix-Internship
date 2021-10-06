06d843b5dd64e83fd41c8a36359c3a6d5f76bde6
Cancer of Japan <leaonardotoshi@gmail.com>
2021-10-06 17:02:11 +0800

    Curretnly has API request limitaion

--- Accounting Assistance/initial_link.py
+++ Accounting Assistance/initial_link.py
@@ -17,22 +17,58 @@ gc = gspread.authorize(credentials)
 
 #KEY of spreadsheet we use
 SPREADSHEET_KEY_MASTER = '1K_VtTBEdDGXuTNcqX8OrdR-LUmgAhqFjWv56CaSGDu0'
-SPREADSHEET_KEY_CHILD = '1dcktFcIv6SyZtP1npbfCGmXl7ulZSvK61EjXpVkaQ-w'
+SPREADSHEET_KEY_CHILD_1 = '1dcktFcIv6SyZtP1npbfCGmXl7ulZSvK61EjXpVkaQ-w'
+SPREADSHEET_KEY_CHILD_2 = '1VqVgJoRiXVn7mq8gh02F1BFPClnQ61wOJWGcfiLoSrE'
 
 #Into work-able condition
 Master_Worksheet = gc.open_by_key(SPREADSHEET_KEY_MASTER).sheet1
-Child_Worksheet = gc.open_by_key(SPREADSHEET_KEY_CHILD).sheet1
+Child_Worksheet = gc.open_by_key(SPREADSHEET_KEY_CHILD_1).sheet1
+Kid_Worksheet = gc.open_by_key(SPREADSHEET_KEY_CHILD_2).sheet1
 #Child_Worksheet = Child_Worksheet.worksheet('シート1')
 
 val = Master_Worksheet.row_values(745)
 print(val)
 Child_Worksheet.update_cell(96,1, val[2])
 
-i = 1
+i = 748
 check = False
+chk_list = ["Child", "Kid"]
+key_list = [Child_Worksheet, Kid_Worksheet]
 while check == False:
     i += 1
-    import_val = str(Master_Worksheet.cell(i,2).value)
-    if import_val == '':
+    import_val = str(Master_Worksheet.cell(i,4).value)
+    print(import_val)
+    
+#Loop to check each company
+    inner_check = False
+    while inner_check == False:
+        import_val = str(Master_Worksheet.cell(i,4).value)
+        x = 0
+        if import_val == chk_list[x]:
+            print('found')
+            child_chk = 3
+            sample = str(key_list[x].cell(3,3).value)
+            print(sample)
+            while str(key_list[x].cell(child_chk,3).value) != 'None':
+                child_chk += 1
+            sleep(1)
+            upd_customer = Master_Worksheet.cell(i,4).value
+            sleep(1)
+            upd_intr_day = Master_Worksheet.cell(i,7).value 
+            sleep(1) 
+            upd_mtg_day = Master_Worksheet.cell(i,9).value
+
+            key_list[x].update_cell(child_chk,3,upd_customer)
+            key_list[x].update_cell(child_chk,4,upd_intr_day)
+            key_list[x].update_cell(child_chk,5,upd_mtg_day)
+
+
+            if import_val == 'None':
+                inner_check == True
+                x += 1
+        if import_val == 'None':
+            check = True
+
+    if import_val == 'None':
         check = True
 print(i)
